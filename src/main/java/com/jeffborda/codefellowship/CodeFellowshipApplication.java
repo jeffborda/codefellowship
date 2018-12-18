8fc4a0a9194cc4cb739c3144910b0ade39acd2dc
Jeff Borda <jeffborda@outlook.com>
2018-12-18 09:22:13 -0800

    initializing project

---
+++ src/main/java/com/jeffborda/codefellowship/CodeFellowshipApplication.java
@@ -0,0 +1,14 @@
+package com.jeffborda.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jeffborda/codefellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeffborda.codefellowship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeFellowshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
